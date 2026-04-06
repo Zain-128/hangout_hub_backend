7e2465ef5eb91026df35b574dfcc77996a4fcd66
Zain Raza <100517579+Zain-128@users.noreply.github.com>
2026-04-06 13:34:37 -0400

    feat:group apis created

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import ErrorHandler from "./middlewares/ErrorHandler.js";
 import userRoutes from "./routes/user.routes.js";
+import groupRoutes from "./routes/group.routes.js";
+import joinRequestRoutes from "./routes/joinRequest.routes.js";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('uploads'));
 
 app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/group", groupRoutes);
+app.use("/api/v1/joinRequest", joinRequestRoutes);
 
 app.use(ErrorHandler);
 
